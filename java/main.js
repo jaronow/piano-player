cc37fc385f176ba12ebd2f4e657970e4be38b5d2
Will Aronow <john.aronow@gmail.com>
2019-05-10 15:47:39 -0700

    created event handler function to move to third progress from second

--- java/main.js
+++ java/main.js
@@ -46,8 +46,14 @@ nextOne.onclick = () => {
   document.getElementById('letter-note-six').innerHTML = 'C';
 };
 
-// Write anonymous event handler property and function for the second progress button
-
+// event handler second progress button to go to third
+nextTwo.onclick = () => {
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
+  document.getElementById('word-five').innerHTML = 'DEAR';
+  document.getElementById('word-six').innerHTML = 'FRI-';
+  lastLyric.style.display = 'inline-block';
+};
 
 // Write anonymous event handler property and function for the third progress button
 
