80b3abc900bdcc6634107c2fc10851daab7384be
Will Aronow <john.aronow@gmail.com>
2019-05-10 16:01:01 -0700

    fixed error in code for last of song

--- java/main.js
+++ java/main.js
@@ -75,7 +75,7 @@ nextThree.onclick = () => {
   document.getElementById('letter-note-four').innerHTML = 'C';
   document.getElementById('letter-note-five').innerHTML = 'D';
   document.getElementById('letter-note-six').innerHTML = 'C';
-  lastLyric.display = 'none';
+  lastLyric.style.display = 'none';
 };
 
 
